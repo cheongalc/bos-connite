0c43356867c797d5136d02239f8737ac37f9156e
Al Cheong <22002891+meeps123@users.noreply.github.com>
2019-05-13 09:28:59 +0800

    Added TouchOpacity to support onPress props

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import {createStackNavigator, createAppContainer} from 'react-navigation'
 import HomePage from './screens/HomePage'
 import AccountInfoPage from './screens/AccountInfoPage'
 
-const MainNavigator = createStackNavigator({
-  HomePage: {screen: HomePage},
-  AccountInfoPage: {screen: AccountInfoPage}
-}, { initialRouteName: 'HomePage' })
+const MainNavigator = createStackNavigator(
+  {
+    HomePage: {screen: HomePage},
+    AccountInfoPage: {screen: AccountInfoPage}
+  },
+  { initialRouteName: 'HomePage' }
+)
 
 const App = createAppContainer(MainNavigator)
 
