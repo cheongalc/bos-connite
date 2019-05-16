86095beb434b436935bf24bb88c7c83200e6bde9
Al Cheong <22002891+meeps123@users.noreply.github.com>
2019-05-17 00:06:08 +0800

    Added logtag to MainActivity

--- app/src/main/java/com/connite/MainActivity.java
+++ app/src/main/java/com/connite/MainActivity.java
@@ -3,6 +3,7 @@ package com.connite;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -12,7 +13,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String logTag = "MAINACTIVITY";
+
     private ExpandableHeightListView ehlv_ActivityItemList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateActivityItemList() {
         ArrayList<ActivityItemData> arrayList = new ArrayList<>();
+        Log.d(logTag, String.valueOf(Uri.parse("https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")));
         for (int i = 0; i < 10; i++) {
             arrayList.add(
                     new ActivityItemData(
@@ -31,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
                             "Test Location",
                             1,
                             1,
-                            Uri.parse("https://yt3.ggpht.com/a/AGF-l7_L7vL6Oy81SONDWdR74cNoObVfod_NsG7OAg=s176-c-k-c0x00ffffff-no-rj-mo")));
+                            Uri.parse("https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")));
         }
         ArrayAdapter<ActivityItemData> adapter = new ActivityItemDataAdapter(this, 0, arrayList);
         ehlv_ActivityItemList = findViewById(R.id.ehlv_ActivityItemList);
