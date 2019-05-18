5dc31f8f1e160731fc29170e45fd3ea8975099a5
Dhaulagiri0 <yiqinw4@gmail.com>
2019-05-18 11:45:15 +0800

    Changed active colour to light green. Changed the active colour changing method.

--- app/src/main/java/com/connite/MainActivity.java
+++ app/src/main/java/com/connite/MainActivity.java
@@ -23,19 +23,22 @@ public class MainActivity extends AppCompatActivity implements FragmentInterface
 
     private static String LOG_TAG = "MAINACTIVITY";
 
-//    Variables for navbar
+    //    Variables for navbar
     private boolean navbarIsOpen = false;
     private View v_translucentHide;
     private RelativeLayout rl_userInfoNavbarContainer;
     private static int mediumAnimationDuration;
 
-//    Variables for fragment management
+    //    Variables for fragment management
     private FragmentManager fragmentManager;
     private RelativeLayout homeFragmentRootLayout,
             connectFragmentRootLayout,
             pastActivitiesFragmentRootLayout,
             settingsFragmentRootLayout;
     private String currentFragment = "";
+
+    private ImageView navBarCurrentActiveIcon;
+    private TextView navBarCurrentActiveText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,49 +49,82 @@ public class MainActivity extends AppCompatActivity implements FragmentInterface
         mediumAnimationDuration = getResources().getInteger(android.R.integer.config_mediumAnimTime);
         v_translucentHide = findViewById(R.id.v_TranslucentEffectMask);
         v_translucentHide.setVisibility(View.GONE);
-        v_translucentHide.setOnClickListener(new View.OnClickListener() { @Override public void onClick(View view) { toggleNavbar(); }});
+        v_translucentHide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleNavbar();
+            }
+        });
         rl_userInfoNavbarContainer = findViewById(R.id.rl_NavbarContainer);
         rl_userInfoNavbarContainer.setVisibility(View.GONE);
         CardView cv_userHeaderCard = findViewById(R.id.cv_UserHeaderCard);
-        cv_userHeaderCard.setOnClickListener(new View.OnClickListener() { @Override public void onClick(View view) { toggleNavbar(); }});
-        RelativeLayout ll_NavbarHomeContainer = findViewById(R.id.ll_NavbarHomeContainer);
-        ll_NavbarHomeContainer.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View view) { showFragment("Home"); }});
-        RelativeLayout ll_NavbarConnectContainer = findViewById(R.id.ll_NavbarConnectContainer);
-        ll_NavbarConnectContainer.setOnClickListener(new View.OnClickListener() { @Override public void onClick(View view) { showFragment("Connect"); }});
-        RelativeLayout ll_NavbarPastActivitiesContainer = findViewById(R.id.ll_NavbarPastActivitiesContainer);
-        ll_NavbarPastActivitiesContainer.setOnClickListener(new View.OnClickListener() { @Override public void onClick(View view) { showFragment("PastActivities"); }});
-        RelativeLayout ll_NavbarSettingsContainer = findViewById(R.id.ll_NavbarSettingsContainer);
-        ll_NavbarSettingsContainer.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View view) { showFragment("Settings"); }});
+        cv_userHeaderCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleNavbar();
+            }
+        });
+        final RelativeLayout ll_NavbarHomeContainer = findViewById(R.id.ll_NavbarHomeContainer);
+        ll_NavbarHomeContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFragment("Home", ll_NavbarHomeContainer);
+            }
+        });
+        final RelativeLayout ll_NavbarConnectContainer = findViewById(R.id.ll_NavbarConnectContainer);
+        ll_NavbarConnectContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFragment("Connect", ll_NavbarConnectContainer);
+            }
+        });
+        final RelativeLayout ll_NavbarPastActivitiesContainer = findViewById(R.id.ll_NavbarPastActivitiesContainer);
+        ll_NavbarPastActivitiesContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFragment("PastActivities", ll_NavbarPastActivitiesContainer);
+            }
+        });
+        final RelativeLayout ll_NavbarSettingsContainer = findViewById(R.id.ll_NavbarSettingsContainer);
+        ll_NavbarSettingsContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFragment("Settings", ll_NavbarSettingsContainer);
+            }
+        });
 
 //        Initialize fragment management
         fragmentManager = getSupportFragmentManager();
-        showFragment("Home");
+        showFragment("Home", ll_NavbarHomeContainer);
 
 //        Initialize suggestions activity
     }
 
-    public void showFragment(String fragmentName) {
-        if (currentFragment.equals(fragmentName)) return ;
+    public void showFragment(String fragmentName, RelativeLayout container) {
+        if (currentFragment.equals(fragmentName)) return;
         currentFragment = fragmentName;
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         Fragment fragment = null;
 
+        ImageView icon = (ImageView) container.getChildAt(0);
+        TextView text = (TextView) container.getChildAt(1);
+
         switch (fragmentName) {
             case "Home":
                 fragment = new HomeFragment();
-                setActiveNavbarTab(0);
+                setActiveNavbarTab(icon, text);
                 break;
             case "Connect":
                 fragment = new ConnectFragment();
-                setActiveNavbarTab(1);
+                setActiveNavbarTab(icon, text);
                 break;
             case "PastActivities":
                 fragment = new PastActivitiesFragment();
-                setActiveNavbarTab(2);
+                setActiveNavbarTab(icon, text);
                 break;
             case "Settings":
                 fragment = new SettingsFragment();
-                setActiveNavbarTab(3);
+                setActiveNavbarTab(icon, text);
                 break;
         }
         transaction.replace(R.id.fl_MainActivityFragmentContainer, fragment);
@@ -96,28 +132,21 @@ public class MainActivity extends AppCompatActivity implements FragmentInterface
         if (navbarIsOpen) toggleNavbar();
     }
 
-    private void setActiveNavbarTab(int tabID) {
-        int activeTabColor = getResources().getColor(R.color.colorNavyBlue);
+    private void setActiveNavbarTab(ImageView icon, TextView text) {
+        int activeTabColor = getResources().getColor(R.color.colorGreenish);
         int colorWhite = getResources().getColor(R.color.colorWhite);
-        int[] navbarHomeTextIDs = {R.id.tv_NavbarHomeText,
-                R.id.tv_NavbarConnectText,
-                R.id.tv_NavbarPastActivitiesText,
-                R.id.tv_NavbarSettingsText};
-        int[] navbarIconIDs = {R.id.iv_NavbarHomeIcon,
-                R.id.iv_NavbarConnectIcon,
-                R.id.iv_NavbarPastActivitiesIcon,
-                R.id.iv_NavbarSettingsIcon};
-        for (int i = 0; i < 4; i++) {
-            TextView currTextView = findViewById(navbarHomeTextIDs[i]);
-            ImageView currImageView = findViewById(navbarIconIDs[i]);
-            if (i == tabID) {
-                currTextView.setTextColor(activeTabColor);
-                currImageView.setColorFilter(activeTabColor);
-            } else {
-                if (currTextView.getCurrentTextColor() == activeTabColor) currTextView.setTextColor(colorWhite);
-                currImageView.setColorFilter(colorWhite);
-            }
+
+        if (navBarCurrentActiveIcon != null && navBarCurrentActiveText != null) {
+            navBarCurrentActiveIcon.setColorFilter(colorWhite);
+            navBarCurrentActiveText.setTextColor(colorWhite);
         }
+
+        icon.setColorFilter(activeTabColor);
+        text.setTextColor(activeTabColor);
+
+        navBarCurrentActiveText = text;
+        navBarCurrentActiveIcon = icon;
+        
     }
 
     public void startSuggestionsActivity(View view) {
